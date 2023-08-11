731b948a1c41df46e42f18f379e37c1654b50e07
arafatrahman41 <arafatrahman41@outlook.com>
2023-08-11 07:39:30 +0600

    added simple cart , quantity cart, and discount file

---
+++ simple-cart.js
@@ -0,0 +1,18 @@
+const shoppingCart = [
+    {name: 'shirt', price: 2200},
+    {name: 'pant', price: 3700},
+    {name: 'shoe', price: 1800},
+    {name: 'shirt', price: 600}
+]
+
+function totalCost(products){
+    let sum = 0;
+    for(let i = 0; i < products.length; i++){
+        const product = products [i];
+        sum = sum + product.price;
+    }
+    return sum;
+}
+
+const expense = totalCost(shoppingCart);
+console.log('Total expense today:',expense);
